52886dd12220090c4eb8d5c57ccebe53b650a33c
karim-w <karim.wael@gmail.com>
2023-09-16 14:37:31 +0400

    fuck u very much xcode

---
+++ sd-ldn/sd_ldnApp.swift
@@ -0,0 +1,17 @@
+//
+//  sd_ldnApp.swift
+//  sd-ldn
+//
+//  Created by Karim Hassan on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct sd_ldnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
